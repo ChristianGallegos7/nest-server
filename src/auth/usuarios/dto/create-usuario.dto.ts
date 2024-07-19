723aac08cd294cdeed22f18ec8c5d3eb3700f4f2
Pxlarized <chriz@liz.com>
2024-07-18 19:12:53 -0700

    Crud de usuarios completado, falta paginar

--- src/auth/usuarios/dto/create-usuario.dto.ts
+++ src/auth/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUsuarioDto {}
+import { IsString, IsEmail, IsNotEmpty, MinLength, MaxLength, IsInt, Min, Max, IsPhoneNumber, Matches } from 'class-validator';
+
+export class CreateUsuarioDto {
+    
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
+    nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
+    apellido: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(20)
+    password: string;
+
+    @IsInt()
+    @Min(0)
+    @Max(120)
+    edad: number;
+
+   
+    @IsNotEmpty({ message: 'El teléfono es obligatorio' })
+    @Matches(/^[0-9]{10}$/, { message: 'El teléfono debe tener 10 dígitos numéricos' })
+    telefono: string;
+}
--- src/auth/usuarios/entities/usuario.entity.ts
+++ src/auth/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,26 @@
-export class Usuario {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Usuario {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 50 })
+    nombre: string;
+
+    @Column({ type: 'varchar', length: 50 })
+    apellido: string;
+
+    @Column({ type: 'varchar', unique: true })
+    email: string;
+
+    @Column({ type: 'varchar' })
+    password: string;
+
+    @Column({ type: 'int' })
+    edad: number;
+
+    @Column({ type: 'varchar', length: 15 })
+    telefono: string;
+}
