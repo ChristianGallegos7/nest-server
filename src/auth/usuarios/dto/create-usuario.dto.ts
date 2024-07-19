31ca0033466393e0b310eb4143e2f7033ad241f7
Pxlarized <christian@moon.ec>
2024-07-18 22:29:37 -0500

    desde laptop

--- src/auth/usuarios/dto/create-usuario.dto.ts
+++ src/auth/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,33 @@
-export class CreateUsuarioDto {}
+import { IsString, IsEmail, IsNotEmpty, MinLength, MaxLength, IsInt, Min, Max, IsPhoneNumber } from 'class-validator';
+
+export class CreateUsuarioDto {
+    
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
+    nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(50)
+    apellido: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(20)
+    password: string;
+
+    @IsInt()
+    @Min(0)
+    @Max(120)
+    edad: number;
+
+    @IsPhoneNumber(null)
+    telefono: string;
+}
--- src/auth/usuarios/entities/usuario.entity.ts
+++ src/auth/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,29 @@
-export class Usuario {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Usuario {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre:string;
+
+    @Column()
+    apellido:string;
+
+    @Column()
+    email:string;
+
+    @Column()
+    password:string;
+
+    @Column()
+    edad:number;
+
+    @Column()
+    telefono:string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+}
