aa4fdf40d145733fe1a936eac26889df69550d2e
Pxlarized <chriz@liz.com>
2024-07-20 12:48:27 -0700

    ya valido que el archivo subido sea solo pdf

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 
+import { fileFilter } from './helpers/fileFilter.helper';
+
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService) { }
 
- @Post('cv')
- @UseInterceptors(FileInterceptor('file'))
- uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-    return 'File uploaded successfully';
+  @Post('cv')
+  // Interceptor para validar que el cv solo se pueda subir como pdf
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter: fileFilter
+  }))
+
+  //Funcion para subir el archivo
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+
+    if(!file){
+      throw new BadRequestException("El cv tiene que ser un pdf")
+    }
+
+    return {
+      fileName: file.originalname
+    }
   }
-  
 }
